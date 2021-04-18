eb44a52d848150e6a9d8b7ee87460078d343140a
Adriano Dijan <adijan00@fesb.hr>
2021-04-18 11:31:23 +0200

    QR

--- frontend/src/api/products.js
+++ frontend/src/api/products.js
@@ -1,4 +1,4 @@
-class Products {
+export default class Products {
 	static async search({search_term, lat, long}) {
 		const response = await fetch(`/api/products?search_term={search_term}&lat={lat}&long={long}`)
 
